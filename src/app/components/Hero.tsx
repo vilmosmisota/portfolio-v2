1592b1fdec547f4f5febdbcd483816b367447030
vilmosmisota <misotavilmos@gmail.com>
2024-10-27 22:38:19 +0000

    change src image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import portfolio from "../../../public/images/portfolio.jpg";
 
 export default function Hero() {
   return (
@@ -35,7 +34,7 @@ export default function Hero() {
           <div className="absolute inset-0 bg-gradient-to-r from-neutral-400 to-orange-200 rounded-2xl rotate-3 opacity-50 blur-xl" />
           <div className="relative h-full w-full overflow-hidden rounded-2xl border-2 border-neutral-700 shadow-2xl">
             <Image
-              src={portfolio}
+              src={"/images/portfolio.jpg"}
               alt="Vilmos Misota"
               placeholder="blur"
               priority
